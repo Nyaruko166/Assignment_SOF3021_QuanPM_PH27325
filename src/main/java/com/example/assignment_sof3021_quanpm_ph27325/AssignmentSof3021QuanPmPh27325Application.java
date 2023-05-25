23a3ef28a41b28ef38874639536180353633917f
Nyaruko <occho1666@gmail.com>
2023-05-25 14:10:52 +0700

    Init

---
+++ src/main/java/com/example/assignment_sof3021_quanpm_ph27325/AssignmentSof3021QuanPmPh27325Application.java
@@ -0,0 +1,13 @@
+package com.example.assignment_sof3021_quanpm_ph27325;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentSof3021QuanPmPh27325Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentSof3021QuanPmPh27325Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assignment_sof3021_quanpm_ph27325/AssignmentSof3021QuanPmPh27325ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment_sof3021_quanpm_ph27325;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSof3021QuanPmPh27325ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
