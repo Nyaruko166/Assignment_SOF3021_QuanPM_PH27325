0f9a59566739efeb9ff2ec97c33ebb7d9b03cae9
Nyaruko <occho1666@gmail.com>
2023-06-13 19:46:44 +0700

    Dunno mam

---
+++ src/main/java/com/example/assignment_sof3021_quanpm_ph27325/repository/HoaDonChiTietRepository.java
@@ -0,0 +1,7 @@
+package com.example.assignment_sof3021_quanpm_ph27325.repository;
+
+import com.example.assignment_sof3021_quanpm_ph27325.entity.HoaDonChiTiet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HoaDonChiTietRepository extends JpaRepository<HoaDonChiTiet, Integer> {
+}
